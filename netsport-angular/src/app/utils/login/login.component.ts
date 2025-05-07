7fba4e6585b937067171eee0dea6edd67406be63
Erick <ericksalazar@javeriana.edu.co>
2025-05-07 17:11:01 -0500

    refactor: Se ajustan ultimos detalles

--- netsport-angular/src/app/utils/login/login.component.ts
+++ netsport-angular/src/app/utils/login/login.component.ts
@@ -22,25 +22,36 @@ export class LoginComponent {
     private router:Router
   ) { }
 
+  msgError:string = "";
+
   ngOnInit(): void {
     this.tipoUsuario = this.route.snapshot.paramMap.get('tipoUsuario');
     console.log(this.arrendadorServicio.findAll());
   }
 
+  camposValidos(): boolean {
+    return this.txtUsuario !== "" && this.txtContrasena !== "";
+  }
+
   login() {
+
+    if(!this.camposValidos()) {
+      this.msgError = "Error: Todos los campos son obligatorios.";
+      return;
+    }
+
+
     switch(this.tipoUsuario) {
       case "jugador": 
         this.jugadorServicio.findByUsuarioAndContrasena(this.txtUsuario,this.txtContrasena).subscribe(j => {
-          j !== undefined ?
-          this.router.navigate([`/jugador/dashboard/${j.id}/perfil/${j.id}`]) :
-          alert("El usuario no existe.");
+          j ? this.router.navigate([`/jugador/dashboard/${j.id}/perfil/${j.id}`]) :
+          this.msgError = "Error: El jugador no se encuentra registrado."
         });
       break;
       case "arrendador":
         this.arrendadorServicio.findByUsuarioAndContrasena(this.txtUsuario,this.txtContrasena).subscribe(a => {
-          a !== undefined ?
-          this.router.navigate([`/arrendador/dashboard/${a.id}/perfil/${a.id}`]) :
-          alert("El arrendador no existe.");
+          a ? this.router.navigate([`/arrendador/dashboard/${a.id}/perfil/${a.id}`]) :
+          this.msgError = "Error: El arrendador no se encuentra registrado."
         });
       break;
       default: alert("Error: no se reconoce el usuario con el que entro al login.");
